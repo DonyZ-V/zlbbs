f64ccdf168ac767222c1d4c9985c98193c58176a
DonyZ <chinazxd1993@163.com>
2018-01-02 19:41:33 +0800

    '#124项目结构搭建'

---
+++ zlbbs.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import config
+from apps.cms import bp as cms_bp
+from apps.common import bp as common_bp
+from apps.front import bp as front_bp
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+app.register_blueprint(cms_bp)
+app.register_blueprint(common_bp)
+app.register_blueprint(front_bp)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
