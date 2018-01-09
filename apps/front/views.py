5001b708d33ae149380615026db75bea26eacbd9
DonyZ <chinazxd1993@163.com>
2018-01-09 18:04:12 +0800

    '#152'

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, views, render_template, redirect, url_for
+from flask import Blueprint, views, render_template, redirect, url_for,make_response
+from utils.captcha import Captcha
+from io import BytesIO
 
 bp = Blueprint('front', __name__)
 
@@ -6,6 +8,16 @@ bp = Blueprint('front', __name__)
 @bp.route('/')
 def index():
     return 'front index'
+
+@bp.route('/captcha/')
+def graph_captcha():
+    text,image = Captcha.gene_graph_captcha()
+    out = BytesIO()
+    image.save(out,'png')
+    out.seek(0)
+    resp = make_response(out.read())
+    resp.content_type = 'image/png'
+    return resp
 
 
 class SignupView(views.MethodView):
